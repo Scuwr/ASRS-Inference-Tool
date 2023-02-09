68a67be4c560564536684546a4e9be34e42c7df1
Christian Arnold <cmarnold@mit.edu>
2023-02-08 22:32:38 -0500

    finally got stable diffusion working

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import redis
 import json
 
+import torch
+from diffusers import StableDiffusionPipeline
+
 r = redis.Redis(port=6397)
 
 database = "v1:"
@@ -13,7 +16,18 @@ def get_annotation(username, storyid):
     userid = [key.decode('utf-8') for key in r.smembers(database + usernames + username)][0].split(':')[2]
     annotation = r.hget(f'{database + annotationtable + userid}:{storyid}', 'annotation')
 
-    data = json.loads(annotation)
-    print(data)
+    return json.loads(annotation)
 
-get_annotation('cmarnold', 1)
+annotation = get_annotation('cmarnold', 1)
+name = annotation['node_names'][1]['name']
+
+model_id = "CompVis/stable-diffusion-v1-4"
+model = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+model = model.to('cuda:0')
+
+for node in annotation['node_names']:
+    prompt = node['name']
+    image = model(prompt).images[0]
+    short_name = " ".join(prompt.split(' ')[:2])
+    print(short_name)
+    image.save(f'inference/{short_name}.png')
